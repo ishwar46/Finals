63308f1e4cefc50d2b2d1cc2eb92272e6ec074cc
ishwar46 <ishwarura@gmail.com>
2022-02-25 07:58:55 +0545

    Update loopssss.py

--- Practice/loopssss.py
+++ Practice/loopssss.py
@@ -11,3 +11,13 @@ list1 = [["Ishu", 20,],
          ["Sneha", 19]]
 for item, age in list1:
     print(item, "is", age,"years old")
+
+#type casting
+hello = [["Ishu", 20,],
+         ["Sam", 20,],
+         ["Roz", 22,],
+         ["Dhawit", 19,],
+         ["Sneha", 19]]
+dict1 = dict(hello)
+for itemnew in dict1:
+    print(itemnew)
