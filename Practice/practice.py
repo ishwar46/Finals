699b6affee2b59031144fedd79c8993bb295cc7d
ishwar46 <ishwarura@gmail.com>
2022-02-25 08:50:16 +0545

    Update practice.py

--- Practice/practice.py
+++ Practice/practice.py
@@ -9,3 +9,21 @@ dictnew = {"Harshika": "Girl",
 print("Enter name to know gender: ")
 result = input()
 print("Entered person is a ",dictnew[result])
+
+#for loop
+
+list1 = [int,float,"ishu","roz",1,2,3,6,7,6,8,"shampoo",["lol",2],"hello"]
+
+for i in list1:
+        if str(i).isnumeric() and i >6:
+                print(i)
+
+#while and break and continue
+
+while(True):
+        i = int(input("Enter any number"))
+        if i<100:
+                print("Try again")
+                continue
+        else:
+                print("Congratulations")
